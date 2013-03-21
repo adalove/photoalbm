f687c2c2ed726271b0960439b57d24cab57ca7c2
Shefali Friesen <musictechgirl@gmail.com>
2013-03-21 09:40:52 -0400

    gallery

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,19 @@
+class PhotosController < ApplicationController
+
+  def index
+  end
+
+
+  def new
+    @photo = Photo.new
+  end
+
+
+  def create
+    photo = Photo.create(params[:photo])
+    album = Album.find(params[:album_id])
+    album.photos << photo
+    @photos = album.photos
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R20130319PhotoAlbm::Application.config.secret_token = '50ff2190bb555b70b9fc8f3231adb917e9e713f857504316f1e82e43b259a09a85c029c4e3a4f840c6149ef27ec788feee282acfe587cbf2925efb7123b5f25a'
